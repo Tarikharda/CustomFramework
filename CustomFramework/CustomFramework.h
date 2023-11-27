30d027e968109f7f2aac5eecf9803856bf31b142
TarikHarda <tarikpy7@gmail.com>
2023-11-27 16:47:47 +0100

    Initial Commit

---
+++ CustomFramework/CustomFramework.h
@@ -0,0 +1,18 @@
+//
+//  CustomFramework.h
+//  CustomFramework
+//
+//  Created by mac book pro on 27/11/2023.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CustomFramework.
+FOUNDATION_EXPORT double CustomFrameworkVersionNumber;
+
+//! Project version string for CustomFramework.
+FOUNDATION_EXPORT const unsigned char CustomFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CustomFramework/PublicHeader.h>
+
+
