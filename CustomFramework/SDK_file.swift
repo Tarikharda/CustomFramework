b1cbdf51ca8b8ec5682f64dbb7c7a52f28742380
TarikHarda <tarikpy7@gmail.com>
2023-11-27 17:00:12 +0100

    first Commit

---
+++ CustomFramework/SDK_file.swift
@@ -0,0 +1,14 @@
+//
+//  SDK_file.swift
+//  CustomFramework
+//
+//  Created by mac book pro on 27/11/2023.
+//
+
+import Foundation
+
+public struct SDK_file{
+    public static func doSomeWork(){
+        print("Do some Work")
+    }
+}
